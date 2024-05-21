d2318bdad1a689859a4c0f126d6df37a1e1fc310
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-05-21 23:17:55 +0600

    next project done

---
+++ myntra-clone/scripts/index.js
@@ -0,0 +1,60 @@
+let bagItems;
+onLoad();
+
+function onLoad(){
+  let bagitemsStr = localStorage.getItem('bagItems');
+  bagItems = bagitemsStr ? JSON.parse(bagitemsStr) : []
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+
+function addToBag(itemId){
+  bagItems.push(itemId)
+  localStorage.setItem('bagItems', JSON.stringify(bagItems))
+  displayBagIcon();
+}
+function displayBagIcon(){
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+
+  if(bagItems.length > 0){
+    bagItemCountElement.style.visibility = "visible"
+    bagItemCountElement.innerHTML = bagItems.length;
+  }else{
+    bagItemCountElement.style.visibility = "hidden"
+  }
+
+}
+function displayItemsOnHomePage(){
+
+  
+let itemsContainerElement = document.querySelector('.items-container');
+
+if(!itemsContainerElement){
+  return;
+}
+
+let innerHTML = '';
+
+items.forEach((item) => {
+    innerHTML += ` <div class="item-container">
+    <img src="${item.image}" alt="item image" class="item-image">
+        <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div>
+        <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">${item.current_price}Rs</span>
+            <span class="original-price">${item.original_price}Rs</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag" onClick="addToBag(${item.id})">Add to Bag</button>
+  </div>
+`
+})
+
+itemsContainerElement.innerHTML = innerHTML;
+}
+
+  
